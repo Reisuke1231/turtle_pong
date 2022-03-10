b026d35f312c82fb7b1a703a9d5f67c99ab1d5c0
Reisuke1231 <reisuke1231@gmail.com>
2022-03-11 08:45:32 +0900

    Restrict paddle move

--- paddle.py
+++ paddle.py
@@ -11,9 +11,12 @@ class Paddle(Turtle):
         self.goto(position)
 
     def go_up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
+        print(self.ycor())
+        if self.ycor() < 240:
+            new_y = self.ycor() + 20
+            self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        new_y = self.ycor() - 20
-        self.goto(self.xcor(), new_y)
+        if self.ycor() > -240:
+            new_y = self.ycor() - 20
+            self.goto(self.xcor(), new_y)
